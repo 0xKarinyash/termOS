f3c1c94a613a5e117b31493b2dd213cfb9f07d0b
Karina <karinyash96@gmail.com>
2025-12-23 16:30:28 +0400

    feat: more fun messages in panic; types are not _BitInt() anymore (C's asm refused to eat that)

--- kernel/include/types.h
+++ kernel/include/types.h
@@ -1,15 +1,15 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef unsigned _BitInt(8)  u8;
-typedef unsigned _BitInt(16) u16;
-typedef unsigned _BitInt(32) u32;
-typedef unsigned _BitInt(64) u64;
+typedef unsigned char      u8;
+typedef unsigned short     u16;
+typedef unsigned int       u32;
+typedef unsigned long long u64; 
 
-typedef signed _BitInt(8)    i8;
-typedef signed _BitInt(16)   i16;
-typedef signed _BitInt(32)   i32;
-typedef signed _BitInt(64)   i64;
+typedef signed char        i8;
+typedef signed short       i16;
+typedef signed int         i32;
+typedef signed long long   i64;
 
 typedef u64 usize;
 typedef u64 uintptr_t;
